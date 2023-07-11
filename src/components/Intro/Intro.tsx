283e24adbc6c0eb70ebf98dc5a0e40c9d2bf0753
Junyu <ericli.jy@outlook.com>
2023-07-11 22:32:23 +0800

    Added placeholder for the first part

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -9,8 +9,17 @@ const Intro = () => {
                     Junyu (Eric) Li           李骏宇
                 </pre>
             </h1>
-            <div>
-                
+            <div className="alpaca-intro">
+                <div className="alpaca-pic">
+
+                </div>
+                <div className="alpaca-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </div>
             </div>
 
         </div>
