1fe104a070808cb9c019fdcad94c13bc53902369
Junyu <ericli.jy@outlook.com>
2023-07-27 22:01:24 +0800

    Started resume editing

---
+++ src/components/ResumeContent/ResumeContent.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ResumeContent.css';
+
+const ResumeContent = () => {
+
+    return(
+        <div>
+            <div className="resume-title">
+                Resume
+            </div>
+
+        </div>
+    )
+}
+
+export default ResumeContent;
