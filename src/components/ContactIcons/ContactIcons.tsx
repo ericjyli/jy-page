b2531b0e58f3f4c1931eaef20e27e1bed44ac3af
Junyu <ericli.jy@outlook.com>
2023-07-27 14:07:48 +0800

    Added contact icons

--- src/components/ContactIcons/ContactIcons.tsx
+++ src/components/ContactIcons/ContactIcons.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import './ContactIcons.css'
 
-import { GithubOutlined, LinkedinOutlined, InstagramOutlined, TwitterOutlined, MailOutlined   } from '@ant-design/icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter, faInstagram, faGithub, faLinkedin, faDiscord } from '@fortawesome/free-brands-svg-icons'
+
 
 const ContactIcons = () => {
 
     return (
         <div className="contact-icons-container">
-            <GithubOutlined />
-            <LinkedinOutlined />
-            <InstagramOutlined />
-            <TwitterOutlined />
-            <MailOutlined />
+            <FontAwesomeIcon icon={faGithub} />
+            <FontAwesomeIcon icon={faLinkedin} />
+            <FontAwesomeIcon icon={faInstagram} />
+            <FontAwesomeIcon icon={faDiscord} />
+            <FontAwesomeIcon icon={faTwitter} />
+            <FontAwesomeIcon icon={faEnvelope} />
+
         </div>
     )
 
