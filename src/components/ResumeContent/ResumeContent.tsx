2363c996d4e8f4bcde36a465fdbed0717065f627
Junyu <ericli.jy@outlook.com>
2023-08-05 18:18:54 +0800

    finished the structure and layout of resume page

--- src/components/ResumeContent/ResumeContent.tsx
+++ src/components/ResumeContent/ResumeContent.tsx
@@ -1,14 +1,42 @@
 import React from 'react';
 import './ResumeContent.css';
 
+import { Typography, Anchor} from 'antd';
+const { Title, Paragraph, Text, Link } = Typography;
+
+
 const ResumeContent = () => {
 
     return(
-        <div>
-            <div className="resume-title">
-                Resume
-            </div>
+        <div className="resume-container">
+            <Title id="resume-title">Resume</Title>
+            <div className="resume-main-container">
+                <div className="resume-main-content-container">
+                    
+                    <div id="education">
+                        <div className="resume-section-title">Education</div>
+                        <div className="resume-section-content">
 
+                        </div>
+                    </div>
+                </div>
+
+                <div className="resume-anchor-container">
+                    <Anchor
+                        replace
+                        affix
+                        offsetTop={100}
+                        
+                        items={[
+                            {
+                                key: 'resume-education',
+                                href: '#education',
+                                title: 'Education'
+                            }
+                        ]}
+                    />
+                </div>
+            </div>
         </div>
     )
 }
