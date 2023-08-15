e2f7615296e73f2cc2fabb7992f1d7a5a40ce6b7
Junyu <ericli.jy@outlook.com>
2023-08-15 14:17:55 +0800

    Start to implement the course component

---
+++ src/components/Courses/Courses.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+
+interface CourseData {
+    index: string;
+    name: string;
+    link: string;
+}
---
+++ src/data/courses.tsx
@@ -0,0 +1,45 @@
+interface course {
+    index: string;
+    name: string;
+    link: string;
+}
+
+const courses: course[] = [
+    {
+        index: 'ECE241',
+        name: 'Digital Systems',
+        link: ''
+    },
+    {
+        index: 'ECE243',
+        name: 'Computer Organization',
+        link: ''
+    },
+    {
+        index: 'ECE297',
+        name: 'Software Design and Communication',
+        link: ''
+    },
+    {
+        index: 'ECE231',
+        name: 'Introduction to Electronics',
+        link: ''
+    },
+    {
+        index: 'ECE326',
+        name: 'Programming Languages',
+        link: '',
+    },
+    {
+        index: 'ECE344',
+        name: 'Operating Systems',
+        link: ''
+    },
+    {
+        index: 'ECE345',
+        name: 'Algorithms and Data Structures',
+        link: ''
+    }
+];
+
+export { courses };
